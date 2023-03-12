0f9879d7252d67147275a16e46d2e6486df29fde
Janan <71359987+Jananzzzz@users.noreply.github.com>
2023-03-12 20:39:01 +0800

    update

--- crawler/search.py
+++ crawler/search.py
@@ -6,8 +6,9 @@ client = tweepy.Client(bearer_token=config.bear_token)
 query = "covid -is:retweet"
 query0 = "covid OR covid19 is:retweet"
 query1 = "covid OR covid19 -is:retweet"
+query2 = "xijinping -is:retweet"
 
-response = client.search_recent_tweets(query=query, 
+response = client.search_recent_tweets(query=query2, 
 tweet_fields=['created_at', 'lang'], 
 user_fields=['profile_image_url'],
 max_results=10, expansions=['author_id'])
@@ -19,7 +20,8 @@ for tweet in response.data:
         user = users[tweet.author_id]
         # print(tweet.id, tweet.text, tweet.lang)
         # user.description
-        print(user.username, user.profile_image_url, "\n")
+        # print(user.username, user.profile_image_url, "\n")
+        print(tweet.text)
         
 
 
