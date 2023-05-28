cd1d6001d33dd65cecf190470e77096ba7532e79
Jananzzzz <bangzhubieren18@gmail.com>
2023-05-28 22:26:05 +0800

    give up check chinese by user info

--- idiotbots_dot_com/check_chinese.py
+++ idiotbots_dot_com/check_chinese.py
@@ -1,7 +1,6 @@
 import sqlite3
-import json
-import numpy as np
 import matplotlib.pyplot as plt
+from pypinyin import lazy_pinyin
 
 def fetch_all_users(database):
     conn = sqlite3.connect(database)
@@ -12,38 +11,49 @@ def fetch_all_users(database):
     conn.close()
     return users
 
-# check name and bio and location whether it contains chinese
-def check_chinese(text):
-    for ch in text:
-        if u'\u4e00' <= ch <= u'\u9fff':
-            return True
-    return False
 
-# check location
-def check_location(location):
-    china_locations = ['jiangsu', 'guangdong', 'macau', 'henan', 'hunan', 'shanghai', 'ningxia', 'taiwan', 'hubei', 'hongkong', 'xinjiang', 'sichuan', 'China', 'gansu', 'guangxi', 'fujian', 'beijing', 'zhejiang', 'guizhou', 'yunnan', 'qinghai', 'shanxi', 'shandong', 'aomen', 'anhui', 'jilin', 'hainan', 'neimenggu', 'chongqing', 'tianjin', 'hebei', 'heilongjiang', 'xizang', 'xianggang', 'liaoning', 'jiangxi', 'tibet']
-    location0 = location.split(",")
-    location1 = location.split(" ")
-    for i in location0:
-        if i.lower() in china_locations:
-            return True
-    for j in location1:
-        if j.lower() in china_locations:
-            return True
-    if location is None:
+def check_chinese(user):
+
+    # check name and bio and location whether it contains chinese
+    def check_chinese_character(text):
+        for ch in text:
+            if u'\u4e00' <= ch <= u'\u9fff':
+                return True
+        return False
+    
+    def check_japanese_character(text):
+        for ch in text:
+            if u'\u0800' <= ch <= u'\u4e00':
+                return True
+        return False
+    
+    def convert_to_pinyin(text):
+        pinyin_text = lazy_pinyin(text)
+    
+    # check location
+    def check_location(location):
+        china_locations = ['guangzhou', 'jiangsu', 'hainan', 'guangdong', 'macau', 'henan', 'hunan', 'shanghai', 'ningxia', 'taiwan', 'hubei', 'hongkong', 'xinjiang', 'sichuan', 'China', 'gansu', 'guangxi', 'fujian', 'beijing', 'zhejiang', 'guizhou', 'yunnan', 'qinghai', 'shanxi', 'shandong', 'aomen', 'anhui', 'jilin', 'hainan', 'neimenggu', 'chongqing', 'tianjin', 'hebei', 'heilongjiang', 'xizang', 'xianggang', 'liaoning', 'jiangxi', 'tibet']
+        china_locations_in_chinese_character = ['广州','江苏', '海南', '广东', '澳门', '河南', '湖南', '上海', '宁夏', '台湾', '湖北', '香港', '新疆', '四川', '中国', '甘肃', '广西', '福建', '北京', '浙江', '贵州', '云南', '青海', '山西', '山东', '澳门', '安徽', '吉林', '海南', '内蒙古', '重庆', '天津', '河北', '黑龙江', '西藏', '香港', '辽宁', '江西', '西藏']
+        # split location by "," and "，"
+        location0 = location.split(",")
+        location1 = location.split(" ")
+        for i in location0:
+            if i.lower() in china_locations or i in china_locations_in_chinese_character:
+                return True
+        for j in location1:
+            if j.lower() in china_locations or j in china_locations_in_chinese_character:
+                return True
+        if location is None:
+            return False
+        return False
+
+    # User['name'], User['bio'], User['location'] contain chinese character but don't contain japanese character, or User['location'] is in china
+    if (check_chinese_character(user['name']) and check_japanese_character(user['name']) == False) or (check_chinese_character(user['bio']) and check_japanese_character(user['bio']) == False) or check_location(user['location']):
         return True
-    if location.lower() in china_locations:
-        return True
-    return False
+    else:
+        return False
 
 # whether it followed the most popular chinese accounts
-def whether_follow_ch(username):
-    with open("idiotbots_dot_com/data/overall_list.json") as f:
-        data = json.load(f)
-        for i in data:
-            if username in data[i]:
-                return True
-
 # check tweets
 
 # check existence of a user
@@ -57,64 +67,48 @@ def check_existence(username):
     else:
         print(f"{username} does not exist!")
 
-def count_chinese():
-    users = fetch_all_users("idiotbots_dot_com/data/profiles/profile2.db")
-    usernamelist = []
-    print(f"total users:{len(users)}")
-    count_chinese = 0
-    for row in users:
-        user = {
-            "name": row[2],
-            "username": row[3],
-            "bio": row[4],
-            "location": row[5],
-            "tweet": row[9],
-            "following": row[10],
-            "followers": row[11],
-        } 
-        if check_chinese(user["name"]) or check_chinese(user["bio"]) or check_chinese(user["location"]) or check_location(user["location"]):
-            count_chinese += 1
-    print(f"chinese users:{count_chinese}")
 
-def distribution():
+def distribution(users):
     follower_list = []
-    following_list = []
-    users = fetch_all_users("idiotbots_dot_com/data/profiles/profile2.db")
     for row in users:
         follower_list.append(row[11])
-        following_list.append(row[10])
-    follower_list = np.array(follower_list)
-    following_list = np.array(following_list)
-    # bins: 0-1000, 1000-2000, 2000-3000, 3000-5000, 5000-10000, 10000-20000, 20000-50000, 50000-100000, 100000-200000, 200000-500000, 500000-inf
-    bins = [0, 1000, 2000, 3000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, float("inf")]
-    follower_hist = np.histogram(follower_list, bins=bins)
-    following_hist = np.histogram(following_list, bins=bins)
-    print(follower_hist)
-    print(following_hist)
-    plt.hist(follower_list, bins=bins)
+    # x_labels: 0-500, 500-1000, 1000-2000, 2000-5000, 5000-10000, 10000-20000, 20000-50000, 50000-100000, 100000-200000, 200000-500000, 500000-inf
+    y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for follower in follower_list:
+        if follower < 500:
+            y[0] += 1
+        elif follower < 1000:
+            y[1] += 1
+        elif follower < 2000:
+            y[2] += 1
+        elif follower < 5000:
+            y[3] += 1
+        elif follower < 10000:
+            y[4] += 1
+        elif follower < 20000:
+            y[5] += 1
+        elif follower < 50000:
+            y[6] += 1
+        elif follower < 100000:
+            y[7] += 1
+        elif follower < 200000:
+            y[8] += 1
+        elif follower < 500000:
+            y[9] += 1
+        else:
+            y[10] += 1
+    x = ["0-0.5k", "0.5k-1k", "1k-2k", "2k-5k", "5k-10k", "10k-20k", "20k-50k", "50k-100k", "100k-200k", "200k-500k", "500k-inf"]
+    # plt to plot
+    plt.bar(x, y)
+    # plt.xticks(rotation=45)
+    plt.yticks(range(0, 300000, 10000))
+    plt.xlabel("followers")
+    plt.ylabel("number of users")
+    plt.title("followers distribution")
+    # add y value upon each bar
+    for a, b in zip(x, y):
+        plt.text(a, b+0.05, '%.0f' % b, ha='center', va= 'bottom',fontsize=11)
     plt.show()
-    plt.hist(following_list, bins=bins)
-    plt.show()
-
-def followers_rank():
-    users = fetch_all_users("idiotbots_dot_com/data/profiles/profile2.db")
-    # rank users by followers
-    userlist = []
-    for row in users:
-        user = {
-            "name": row[2],
-            "username": row[3],
-            "bio": row[4],
-            "location": row[5],
-            "tweet": row[9],
-            "following": row[10],
-            "followers": row[11],
-        } 
-        userlist.append(user)
-    userlist.sort(key=lambda x: x["followers"], reverse=False)
-    for i in range(100):
-        print(userlist[i]["username"], userlist[i]["followers"]) 
-
 
 if __name__=="__main__":
     not_exist = [
@@ -123,5 +117,26 @@ if __name__=="__main__":
         'nmslesebot',
         'realbingbingfan',
     ]
-    check_existence("realbingbingfan")
+    # check_existence("whyyoutouzhele")
+
     
+    users = fetch_all_users("idiotbots_dot_com/data/profiles/profile2.db")
+    # distribution(users)
+    chinese_users = []
+    for row in users:
+        user = {
+            "name": row[2],
+            "username": row[3],
+            "bio": row[4],
+            "location": row[5],
+            "tweet": row[9],
+            "following": row[10],
+            "followers": row[11],
+        } 
+        if check_chinese(user):
+            chinese_users.append(row)
+    print(f"chinese users:{len(chinese_users)}")
+    # distribution(chinese_users)
+    chinese_users.sort(key=lambda x: x[11], reverse=True)
+    for i in range(100):
+        print(chinese_users[i][2], chinese_users[i][3], chinese_users[i][11])
